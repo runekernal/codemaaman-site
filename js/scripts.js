37b381d4d797f136f89094a1165cdd250d750583
Rune Kernal <runekernal@gmail.com>
2025-02-20 18:01:28 +0000

    - Redefined Home - Added elements for About - fixed dark mode (require further modifications)

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@
 // Dark Mode Toggle
 let isDarkModeOn = false;
 const darkModeIcon = document.getElementById('dark-mode-icon');
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-darkModeToggle.addEventListener('click', () => {
+darkModeIcon.addEventListener('click', () => {
   isDarkModeOn = !isDarkModeOn;
   if (isDarkModeOn) {
-    document.documentElement.style.setProperty("--background-color", "#black");
+    document.documentElement.style.setProperty("--background-color", "black");
     document.documentElement.style.setProperty("--primary-font-color", "#EEEEEE");
     darkModeIcon.classList.replace('ri-moon-line', 'ri-sun-line');
 
@@ -29,17 +28,31 @@ window.addEventListener("scroll", function () {
   
 
 // TypeWriter
+const dynamicText = document.querySelector('.dynamic-text');
+const words = ["Grow.", "Innovate.", "Succeed."];
+let wordIndex = 0;
+let charIndex = 0;
 
-const speed = 100;
-const words = ["Grow.", "Innovate.", "Collaborate."]
-const dynamicText = document.querySelector(".dynamic-text");
-
-function typeWriter() {
-  for (let word of words) {
-    for (let i = 0; i < words.length; i++) {
-      setTimeout(() => {
-        dynamicText.innerHTML += word[i]
-      }, speed);
-    }
+const typeEffect = () => {
+  if (charIndex < words[wordIndex].length) {
+    dynamicText.textContent += words[wordIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else {
+    setTimeout(eraseEffect, 1000);
   }
-}
+};
+
+
+const eraseEffect = () => {
+  if (charIndex > 0) {
+    dynamicText.textContent = words[wordIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseEffect, 50);
+  } else {
+    wordIndex = (wordIndex + 1) % words.length;
+    setTimeout(typeEffect, 500);
+  }
+};
+
+typeEffect();
